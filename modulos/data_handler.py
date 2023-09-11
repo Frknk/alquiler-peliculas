629f03533bb6f70a0b484f009b726316183732ec
Frknk <frknk1@protonmail.com>
2023-09-11 17:38:26 -0500

    Formato

--- modulos/data_handler.py
+++ modulos/data_handler.py
@@ -8,14 +8,16 @@ YML - YAML Ain't Markup Language
 
 import os
 import yaml
-
 import pelicula
+
 
 class Handler:
 
+    """
+    Constructor de la clase Handler
+    """
     def __init__(self):
-        self.root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')) # Obtener directorio raiz
-
+        self.root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Obtener directorio raiz
 
     def crear_data(self):
 
@@ -24,14 +26,15 @@ class Handler:
         Crear peliculas_data.yml, usuarios_data.yml y encargados_data.yml
 
         -> data
-            -> peliculas_data.yml
-            -> usuarios_data.yml
-            -> encargados_data.yml
+            - peliculas_data.yml
+            - usuarios_data.yml
+            - encargados_data.yml
         """
 
         # Si el folder data no existe, crearlo
         if not os.path.exists(os.path.join(self.root_dir, 'data')):
             os.mkdir(os.path.join(self.root_dir, 'data'))
+
         # Si el archivo peliculas_data.yml no existe, crearlo
         if not os.path.exists(os.path.join(self.root_dir, 'data', 'peliculas_data.yml')):
             with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'w') as archivo:
@@ -40,36 +43,38 @@ class Handler:
         # Si el archivo usuarios_data.yml no existe, crearlo
         if not os.path.exists(os.path.join(self.root_dir, 'data', 'usuarios_data.yml')):
             with open(os.path.join(self.root_dir, 'data', 'usuarios_data.yml'), 'w') as archivo:
-                archivo.write('usuarios:') 
+                archivo.write('usuarios:')
+
         # Si el archivo encargados_data.yml no existe, crearlo
         if not os.path.exists(os.path.join(self.root_dir, 'data', 'encargados_data.yml')):
             with open(os.path.join(self.root_dir, 'data', 'encargados_data.yml'), 'w') as archivo:
                 archivo.write('encargados:')
 
     def ingresar_pelicula(self, Pelicula):
-        
+
         """
         Ingresar pelicula al archivo peliculas_data.yml
 
         -> data
             -> peliculas_data.yml
                 -> pelicula_id
-                    -> nombre
-                    -> genero
-                    -> duracion
-                    -> clasificacion
-                    -> idioma
-                    -> subtitulos
-                    -> sinopsis
-                    -> director
-                    -> actores
-                    -> productora
-                    -> pais
-                    -> anio
+                    - nombre
+                    - genero
+                    - duracion
+                    - clasificacion
+                    - idioma
+                    - subtitulos
+                    - sinopsis
+                    - director
+                    - actores
+                    - productora
+                    - pais
+                    - anio
         """
 
         # Crear pelicula_id
         pelicula_id = Pelicula.id
+
         # Crear diccionario de pelicula
         pelicula_dict = {
             pelicula_id: {
@@ -87,62 +92,66 @@ class Handler:
                 'anio': Pelicula.anio
             }
         }
+
         # Abrir peliculas_data.yml
         with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'r') as archivo:
             # Cargar peliculas_data.yml
             peliculas_data = yaml.safe_load(archivo)
             if peliculas_data['peliculas'] is None:
                 peliculas_data['peliculas'] = {}
+
         # Agregar pelicula_dict a peliculas_data
         peliculas_data['peliculas'].update(pelicula_dict)
+
         # Abrir peliculas_data.yml
         with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'w') as archivo:
             # Guardar peliculas_data.yml
             yaml.dump(peliculas_data, archivo)
 
     def eliminar_pelicula(self, pelicula_id):
-            
-            """
+
+        """
             Eliminar pelicula del archivo peliculas_data.yml
     
             -> data
                 -> peliculas_data.yml 
                     -> pelicula_id  (X)
-                        -> nombre
-                        -> genero
-                        -> duracion
-                        -> clasificacion
-                        -> idioma
-                        -> subtitulos
-                        -> sinopsis
-                        -> director
-                        -> actores
-                        -> productora
-                        -> pais
-                        -> anio
-            """
+                        - nombre
+                        - genero
+                        - duracion
+                        - clasificacion
+                        - idioma
+                        - subtitulos
+                        - sinopsis
+                        - director
+                        - actores
+                        - productora
+                        - pais
+                        - anio
+        """
 
-            # Abrir peliculas_data.yml
-            with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'r') as archivo:
-                # Cargar peliculas_data.yml
-                peliculas_data = yaml.safe_load(archivo)
+        # Abrir peliculas_data.yml
+        with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'r') as archivo:
+            # Cargar peliculas_data.yml
+            peliculas_data = yaml.safe_load(archivo)
 
-            # Verificar si pelicula_id existe en peliculas_data
-            if pelicula_id not in peliculas_data['peliculas']:
-                return
-        
-            # Eliminar pelicula_id de peliculas_data
-            peliculas_data['peliculas'].pop(pelicula_id)
+        # Verificar si pelicula_id existe en peliculas_data
+        if pelicula_id not in peliculas_data['peliculas']:
+            return
 
-            # Abrir peliculas_data.yml
-            with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'w') as archivo:
-                # Guardar peliculas_data.yml
-                yaml.dump(peliculas_data, archivo)
+        # Eliminar pelicula_id de peliculas_data
+        peliculas_data['peliculas'].pop(pelicula_id)
+
+        # Abrir peliculas_data.yml
+        with open(os.path.join(self.root_dir, 'data', 'peliculas_data.yml'), 'w') as archivo:
+            # Guardar peliculas_data.yml
+            yaml.dump(peliculas_data, archivo)
 
 
 if __name__ == '__main__':
     handler = Handler()
     handler.crear_data()
-    pelicula = pelicula.Pelicula('nombre', 'genero', 'duracion', 'clasificacion', 'idioma', 'subtitulos', 'sinopsis', 'director', 'actores', 'productora', 'pais', 'anio')
+    pelicula = pelicula.Pelicula('nombre', 'genero', 'duracion', 'clasificacion', 'idioma', 'subtitulos', 'sinopsis',
+                                 'director', 'actores', 'productora', 'pais', 'anio')
     handler.ingresar_pelicula(pelicula)
     handler.eliminar_pelicula('211616')
--- modulos/pelicula.py
+++ modulos/pelicula.py
@@ -2,8 +2,37 @@ import uuid
 import os
 import yaml
 
-class Pelicula:
 
+def generar_id():
+    return str(uuid.uuid4().int)[:6]
+
+
+def crear_id_unica():
+    """
+    Checar si el id ya existe
+    Si existe, crear otro
+    Si no existe, retornar el id
+    """
+
+    data_file = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'data',
+                             'peliculas_data.yml')
+
+    if not os.path.exists(data_file):
+        pass
+
+    with open(data_file, 'r') as archivo:
+        data = yaml.load(archivo, Loader=yaml.FullLoader)
+
+    try:
+        while True:
+            id_gen = generar_id()
+            if id_gen not in data['peliculas']:
+                return id_gen
+    except TypeError:  # Caution
+        return generar_id()  # Si no hay peliculas, retornar un id
+
+
+class Pelicula:
     nombre = ""
     genero = ""
     duracion = ""
@@ -17,8 +46,9 @@ class Pelicula:
     pais = ""
     anio = ""
 
-    def __init__(self, nombre, genero, duracion, clasificacion, idioma, subtitulos, sinopsis, director, actores, productora, pais, anio):
-        self.id = self.crear_id_unica()
+    def __init__(self, nombre, genero, duracion, clasificacion, idioma, subtitulos, sinopsis, director, actores,
+                 productora, pais, anio):
+        self.id = crear_id_unica()
         self.nombre = nombre
         self.genero = genero
         self.duracion = duracion
@@ -31,30 +61,3 @@ class Pelicula:
         self.productora = productora
         self.pais = pais
         self.anio = anio
-
-    def generar_id(self):
-        return str(uuid.uuid4().int)[:6]
-
-    def crear_id_unica(self):
-        """ 
-        Checar si el id ya existe
-        Si existe, crear otro
-        Si no existe, retornar el id
-        """
-
-        data_file = os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')), 'data', 'peliculas_data.yml')
-
-        if not os.path.exists(data_file):
-            pass
-
-        with open(data_file, 'r') as archivo:
-            data = yaml.load(archivo, Loader=yaml.FullLoader)
-    
-        try:
-            while True:
-                id = self.generar_id()
-                if id not in data['peliculas']:
-                    return id
-        except TypeError:
-            return self.generar_id() # Si no hay peliculas, retornar un id
-                
