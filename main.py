2c1f13f70a9e1eda6b1549d5d3be90a01e44d684
Frknk <frknk1@protonmail.com>
2023-09-15 23:17:51 -0500

    Removiendo clase innecesaria

--- main.py
+++ main.py
@@ -41,23 +41,6 @@ def generar_tabla_rich(objeto):
 
 
 # Mostrar datos de pelicula
-table = Table(show_edge=True, show_header=False, box=box.MINIMAL)
-table.add_row("[magenta bold] Genero", pelicula.genero)
-table.add_row("[magenta bold] Duracion", pelicula.duracion)
-table.add_row("[magenta bold] Clasificacion", pelicula.clasificacion)
-table.add_row("[magenta bold] Idioma", pelicula.idioma)
-table.add_row("[magenta bold] Subtitulos", pelicula.subtitulos)
-table.add_row("[magenta bold] Sinopsis", pelicula.sinopsis)
-table.add_row("[magenta bold] Director", pelicula.director)
-table.add_row("[magenta bold] Actores", pelicula.actores)
-table.add_row("[magenta bold] Productora", pelicula.productora)
-table.add_row("[magenta bold] Pais", pelicula.pais)
-table.add_row("[magenta bold] Fecha", pelicula.fecha)
-
 rconsole = rich.get_console()
-rconsole.print(
-    Columns([Panel(table, title=pelicula.nombre)], equal=True), justify="center"
-)
-
 tabla_test = generar_tabla_rich(cliente)
 rconsole.print(tabla_test, justify="center")
